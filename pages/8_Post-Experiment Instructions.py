6d9b3a7791db8108a8fe7c369d1685e715b52103
Vicky1isme <liv8872@gmail.com>
2024-11-26 18:00:51 +0800

    Update 8_Post-Experiment Instructions.py

--- pages/8_Post-Experiment Instructions.py
+++ pages/8_Post-Experiment Instructions.py
@@ -2,13 +2,17 @@ import streamlit as st
 import sqlite3
 import os
 
-def main():
+def post_experiment_page():
     st.title("Post-Experiment Instructions")
 
     # Provide experiment details
     st.markdown("""
-    Thank you for completing this experiment!  
-    As a token of our appreciation, we will provide you with a small candy gift.  
-    Additionally, we would like to clarify that the "group bonus" mentioned during the experiment is purely a part of the experimental design and does not have any actual impact based on the evaluation results with the robot.  
-    Thank you again for your participation! Do you have any questions?
+    Thank you for completing this experiment!\n  
+    As a token of our appreciation, we will provide you with a small candy gift.\n  
+    Additionally, we would like to clarify that the **group bonus** mentioned during the experiment is purely a part of the experimental design and **does not** have any actual impact based on the evaluation results with the robot.  
+    \n Thank you again for your participation! Do you have any questions?
     """)
+
+# Main entry point
+if __name__ == "__main__":
+    post_experiment_page()
